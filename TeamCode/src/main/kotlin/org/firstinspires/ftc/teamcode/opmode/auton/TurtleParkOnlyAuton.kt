637299e98eba960a685d3fdcd27b8fd477ac0155
Bill Broughton <wlbrough@gmail.com>
2024-10-26 10:15:23 -0400

    Add park only autons

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/auton/TurtleParkOnlyAuton.kt
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmode.auton
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.arcrobotics.ftclib.command.ParallelCommandGroup
+import com.arcrobotics.ftclib.command.SequentialCommandGroup
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.firstinspires.ftc.teamcode.command.FollowTrajectorySequence
+import org.firstinspires.ftc.teamcode.command.specimen.HighChamberPosition
+import org.firstinspires.ftc.teamcode.command.specimen.PickupPosition
+import org.firstinspires.ftc.teamcode.command.transfer.PositionDrive
+import org.firstinspires.ftc.teamcode.opmode.OpModeBase
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence
+
+@Autonomous(group = "CyberDragons")
+class TurtleParkOnlyAuton : OpModeBase() {
+    private val heading = Math.toRadians(270.0)
+    private val startingX = -16.0
+    private val startingY = 63.5
+
+    private val parkX = -48.0
+    private val parkY = 60.0
+
+    override fun initialize() {
+        initHardware()
+
+        val startingPose = Pose2d(startingX, startingY, heading)
+        val parkPose = Pose2d(parkX, parkY, heading)
+        val parkTrajectorySequence = mecanumDrive.trajectorySequenceBuilder(startingPose)
+            .lineToLinearHeading(parkPose)
+            .build()
+
+        mecanumDrive.poseEstimate = startingPose
+
+        schedule(
+            SequentialCommandGroup(
+                PositionDrive(viperArmSubsystem, activeIntakeSubsystem),
+                FollowTrajectorySequence(mecanumDrive, parkTrajectorySequence)
+            )
+        )
+    }
+}
