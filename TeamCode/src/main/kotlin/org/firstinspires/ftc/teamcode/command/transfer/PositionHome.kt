fc0875d57ea77333985e52868a2624c1f30d0b93
Bill Broughton <wlbrough@gmail.com>
2024-10-30 20:15:53 -0400

    Update PositionHome command

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/command/transfer/PositionHome.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/command/transfer/PositionHome.kt
@@ -8,11 +8,11 @@ import org.firstinspires.ftc.teamcode.subsystem.ViperArmSubsystem
 class PositionHome(viperArmSubsystem: ViperArmSubsystem, activeIntakeSubsystem: ActiveIntakeSubsystem) : SequentialCommandGroup() {
     init {
         addCommands(
+            RetractHome(viperArmSubsystem),
             ParallelCommandGroup(
+                RotateHome(viperArmSubsystem),
                 WristToPickup(activeIntakeSubsystem),
-                RetractHome(viperArmSubsystem)
-            ),
-            RotateTo(viperArmSubsystem, ViperArmSubsystem.ROTATION_MIN_POSITION)
+            )
         )
         addRequirements(viperArmSubsystem, activeIntakeSubsystem)
     }
