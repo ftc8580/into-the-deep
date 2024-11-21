e0952c2dc2fec6d2bf177cdcaf2a0cb1d84a3e32
Bill Broughton <wlbrough@gmail.com>
2024-11-20 20:31:49 -0500

    Optimize turtle max auton

--- MeepMeepTesting/src/main/java/org/ftc8580/meepmeeptesting/TurtleMaxAuton.kt
+++ MeepMeepTesting/src/main/java/org/ftc8580/meepmeeptesting/TurtleMaxAuton.kt
@@ -19,45 +19,55 @@ fun main(args: Array<String>) {
     val action = bot.drive.actionBuilder(
         Pose2d(-8.0, 63.0, Math.toRadians(270.0))
     )
-        .waitSeconds(1.2)
-        .lineToY(33.0)
-        .waitSeconds(1.0)
-        .lineToY(38.0)
+        .waitSeconds(1.0) // Wait for gripper to raise
+        .lineToY(33.0) // Drive to first delivery position
+        .waitSeconds(0.25) // Wait for gripper to deliver
+        .setTangent(Math.toRadians(150.0)) // Set exit direction
+        .splineToConstantHeading(Vector2d(-37.0, 24.0), Math.toRadians(270.0)) // Spline around submersible corner
+        .setTangent(Math.toRadians(270.0)) // Continue moving in the correct direction
+        .splineToConstantHeading(Vector2d(-47.5, 12.0), Math.toRadians(90.0)) // Continue spline to position behind first sample
+        .splineToConstantHeading(Vector2d(-47.5, 53.0), Math.toRadians(90.0)) // Push first sample to observation zone
         .setTangent(Math.toRadians(90.0))
-        .splineToConstantHeading(Vector2d(-37.0, 22.0), Math.toRadians(270.0))
-//        .lineToY(14.0)
-//        .strafeToConstantHeading(Vector2d(-47.5, 14.0))
+        .splineToConstantHeading(Vector2d(-50.5, 53.0), Math.toRadians(270.0)) // Make a short loop to head back to pickup
+        .splineToConstantHeading(Vector2d(-50.5, 12.0), Math.toRadians(270.0)) // Drive back to next pickup
         .setTangent(Math.toRadians(270.0))
-        .splineToConstantHeading(Vector2d(-47.5, 14.0), Math.toRadians(90.0))
-        .strafeToConstantHeading(Vector2d(-47.5, 53.0))
-        .strafeToConstantHeading(Vector2d(-47.5, 14.0))
-        .strafeToConstantHeading(Vector2d(-57.5, 14.0))
-        .strafeToConstantHeading(Vector2d(-57.5, 53.0))
-        .strafeToConstantHeading(Vector2d(-57.5, 45.0)) // Remove if uncommenting the other lines
-        .waitSeconds(0.25)
-//            .strafeToConstantHeading(Vector2d(-57.5, 12.0))
-//            .strafeToConstantHeading(Vector2d(-63.0, 12.0))
-//            .strafeToConstantHeading(Vector2d(-63.0, 53.0))
-//            .strafeToConstantHeading(Vector2d(-63.0, 48.0))
-        .strafeToLinearHeading(Vector2d(-40.0, 58.0), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-57.5, 16.0), Math.toRadians(90.0)) // Make a short loop to position behind second sample
+        .splineToConstantHeading(Vector2d(-57.5, 53.0), Math.toRadians(90.0)) // Push second sample to the observation area
+//        .setTangent(Math.toRadians(90.0))
+//        .splineToConstantHeading(Vector2d(-59.5, 53.0), Math.toRadians(270.0)) // Make a short loop to head back to pickup
+//        .splineToConstantHeading(Vector2d(-59.5, 12.0), Math.toRadians(270.0)) // Drive back to next pickup
+//        .setTangent(Math.toRadians(270.0))
+//        .splineToConstantHeading(Vector2d(-63.0, 16.0), Math.toRadians(90.0)) // Make a short loop to position behind third sample
+//        .splineToConstantHeading(Vector2d(-63.0, 53.0), Math.toRadians(90.0)) // Push third sample to the observation area
+        .splineToLinearHeading(Pose2d(-40.0, 48.0, Math.toRadians(90.0)), Math.toRadians(90.0))
         .waitSeconds(1.0)
-        .strafeToConstantHeading(Vector2d(-40.0, 63.0))
-        .waitSeconds(0.5)
-        .setTangent(0.0)
-        .splineToSplineHeading(Pose2d(-7.0, 33.0, Math.toRadians(-90.0)), Math.toRadians(-90.0))
-        .waitSeconds(0.5)
-        .strafeToConstantHeading(Vector2d(-7.0, 38.0))
-        .setTangent(Math.toRadians(90.0))
-        .splineToLinearHeading(Pose2d(-40.0, 58.0, Math.toRadians(90.0)), Math.toRadians(90.0))
-        .waitSeconds(1.0)
-        .strafeToConstantHeading(Vector2d(-40.0, 63.0))
-        .waitSeconds(0.5)
-//        .strafeToConstantHeading(Vector2d(-6.0, 38.0))
-        .setTangent(0.0)
+        .splineToConstantHeading(Vector2d(-40.0, 63.0), Math.toRadians(90.0))
+        .waitSeconds(0.4)
+        .setTangent(-45.0)
         .splineToSplineHeading(Pose2d(-6.0, 33.0, Math.toRadians(-90.0)), Math.toRadians(-90.0))
-        .waitSeconds(0.5)
+        .waitSeconds(0.25)
         .setTangent(Math.toRadians(90.0))
-        .splineToLinearHeading(Pose2d(-40.0, 58.0, Math.toRadians(90.0)), Math.toRadians(90.0))
+        .splineToLinearHeading(Pose2d(-40.0, 48.0, Math.toRadians(90.0)), Math.toRadians(90.0))
+        .waitSeconds(1.0)
+//        .splineToConstantHeading(Vector2d(-6.0, 38.0), Math.toRadians(90.0))
+        .setTangent(Math.toRadians(90.0))
+        .splineToLinearHeading(Pose2d(-40.0, 56.0, Math.toRadians(90.0)), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-40.0, 63.0), Math.toRadians(90.0))
+        .waitSeconds(0.4)
+        .setTangent(-45.0)
+        .splineToSplineHeading(Pose2d(-4.0, 33.0, Math.toRadians(-90.0)), Math.toRadians(-90.0))
+        .waitSeconds(0.25)
+        .setTangent(Math.toRadians(90.0))
+        .splineToLinearHeading(Pose2d(-40.0, 48.0, Math.toRadians(90.0)), Math.toRadians(90.0))
+        .waitSeconds(1.0)
+        .splineToConstantHeading(Vector2d(-40.0, 63.0), Math.toRadians(90.0))
+        .waitSeconds(0.4)
+        .setTangent(-45.0)
+        .splineToSplineHeading(Pose2d(-2.0, 33.0, Math.toRadians(-90.0)), Math.toRadians(-90.0))
+        .waitSeconds(0.25)
+//        .splineToConstantHeading(Vector2d(-2.0, 38.0), Math.toRadians(90.0))
+        .setTangent(Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-56.0, 58.0), Math.toRadians(180.0))
         .build()
 
     bot.runAction(action)
